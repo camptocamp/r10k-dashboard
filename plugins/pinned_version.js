06e275e5a1b48fe2643ca14774b08eed6f24295d
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-02-25 17:44:56 +0100

    Add links to pinned_versions.js

--- plugins/pinned_version.js
+++ plugins/pinned_version.js
@@ -1,10 +1,20 @@
 dashboard.pinned_version = function(name, repo) {
   var info = repositories[name].info;
   var version;
+  var url;
   if (info.ref) {
-    var version = info.ref;
+    version = info.ref;
+    if (/^[a-z0-9]+$/.test(version)) {
+      // Assume commit
+      url = info.git+'/commit/'+version;
+    } else {
+      // Assume tag (or branch?)
+      url = info.git+'/releases/tag/'+version;
+    }
   } else {
-    var version = info.version;
+    version = info.version;
+    url = 'https://forge.puppetlabs.com/'+info.forge.current_release.module.owner.username+'/'+info.forge.name;
   }
-  updateCell(name, 'pinned_version', version);
+  var html = '<a href="'+url+'">'+version+'</a>';
+  updateCell(name, 'pinned_version', html);
 }

ed25dd7033c32f464498d5e46741fd5917e5025d
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-02-25 17:46:34 +0100

    Fix version link for forge

--- plugins/pinned_version.js
+++ plugins/pinned_version.js
@@ -1,19 +1,19 @@
 dashboard.pinned_version = function(name, repo) {
-  var info = repositories[name].info;
+  var r = repositories[name];
   var version;
   var url;
   if (info.ref) {
-    version = info.ref;
+    version = r.info.ref;
     if (/^[a-z0-9]+$/.test(version)) {
       // Assume commit
-      url = info.git+'/commit/'+version;
+      url = r.info.git+'/commit/'+version;
     } else {
       // Assume tag (or branch?)
-      url = info.git+'/releases/tag/'+version;
+      url = r.info.git+'/releases/tag/'+version;
     }
   } else {
-    version = info.version;
-    url = 'https://forge.puppetlabs.com/'+info.forge.current_release.module.owner.username+'/'+info.forge.name;
+    version = r.info.version;
+    url = 'https://forge.puppetlabs.com/'+r.forge.current_release.module.owner.username+'/'+r.forge.name+'/'+version;
   }
   var html = '<a href="'+url+'">'+version+'</a>';
   updateCell(name, 'pinned_version', html);

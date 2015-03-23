95cff6705ef700a791cbe46d5a370b0d83af38dd
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-03-23 13:37:13 +0100

    Simplify signature of checkForgeCommits

--- plugins/latest_version.js
+++ plugins/latest_version.js
@@ -16,14 +16,14 @@ dashboard.latest_version = function(name, repo) {
           html += ' <a href="'+r.info.tags_url+'" title="Failed to get tags"><i class="fa fa-warning"></i></a>';
           updateCell(name, 'latest_version', html);
         } else {
-          var base_ref;
+          var new_ref;
           if (r.info.ref) {
             // Use ref as base
-            base_ref = r.info.ref;
+            new_ref = r.info.ref;
           } else {
-            base_ref_tag = versionTagURL(tags, r.info.version);
+            new_ref_tag = versionTagURL(tags, r.info.version);
             if (base_ref_tag) {
-              base_ref = base_ref_tag.tag;
+              new_ref = new_ref_tag.tag;
             } else {
               // No tag found, it's a warning
               html += ' <a href="'+r.info.tags_url+'" title="No matching tag '+r.info.version+' found in repository"><i class="fa fa-warning"></i></a>';
@@ -34,7 +34,7 @@ dashboard.latest_version = function(name, repo) {
           if (version_tag) {
             var html = '<a href="'+version_url+'">'+version+'</a>';
             html += ' <a href="'+version_tag.url+'" title="Matching tag found in repository"><i class="fa fa-tag"></i></a>';
-            checkForgeCommits(name, r, version, version_url, version_tag.tag, r.github.user, base_ref, html);
+            checkForgeCommits(name, r, version, r.github.user, version_tag.tag, new_ref, html);
           } else {
             // No tag found, it's a warning
             html += ' <a href="'+r.info.tags_url+'" title="No matching tag '+version+' found in repository"><i class="fa fa-warning"></i></a>';
@@ -48,40 +48,40 @@ dashboard.latest_version = function(name, repo) {
     var version = 'master';
     var version_url = r.github.uri;
     html = '<a href="'+version_url+'">'+version+'</a>';
-    checkForgeCommits(name, r, version, version_url, 'master', r.github.user, r.info.ref, html);
+    checkForgeCommits(name, r, version, r.github.user, version, r.info.ref, html);
   }
 }
 
-function checkForgeCommits(name, tags_r, version, url, base_version, ref_user, b, html) {
+function checkForgeCommits(name, tags_r, version, ref_user, base_ref, new_ref, html) {
   var state;
   var customkey;
 
   // get diff
-  tags_r.repo.compare(tags_r.github.user+':'+base_version, ref_user+':'+b, function(err, diff) {
+  tags_r.repo.compare(tags_r.github.user+':'+base_ref, ref_user+':'+new_ref, function(err, diff) {
     if (err) {
       html += ' <span title="Failed to get commits since tag"><i class="fa fa-warning"></i></span>';
       updateCell(name, 'latest_version', 'status', html, 'err', '15');
     } else {
       if (diff.status == 'ahead') {
         diff_url = diff.html_url;
-        html += ' <a href="'+diff_url+'" title="Branch '+b+' is '+diff.ahead_by+' commits ahead of tag '+version+'"><i class="fa fa-angle-double-up"></i></a>';
+        html += ' <a href="'+diff_url+'" title="Branch '+new_ref+' is '+diff.ahead_by+' commits ahead of tag '+version+'"><i class="fa fa-angle-double-up"></i></a>';
         state = 'warn';
         customkey = '11';
       } else if (diff.status == 'behind') {
         // /!\ using invertDiffURL from status plugin
         diff_url = invertDiffURL(diff.html_url);
-        html += ' <a href="'+diff_url+'" title="Branch '+b+' is '+diff.behind_by+' commits behind of tag '+version+'"><i class="fa fa-angle-double-down"></i></a>';
+        html += ' <a href="'+diff_url+'" title="Branch '+new_ref+' is '+diff.behind_by+' commits behind of tag '+version+'"><i class="fa fa-angle-double-down"></i></a>';
         state = 'warn';
         customkey = '12';
       } else if (diff.status == 'diverged') {
         diff_url = diff.html_url;
-        html += ' <a href="'+diff_url+'" title="Branch '+b+' is '+diff.behind_by+' commits behind and '+diff.ahead_by+' commits ahead of tag '+version+'"><i class="fa fa-code-fork"></i></a>';
+        html += ' <a href="'+diff_url+'" title="Branch '+new_ref+' is '+diff.behind_by+' commits behind and '+diff.ahead_by+' commits ahead of tag '+version+'"><i class="fa fa-code-fork"></i></a>';
       } else if (diff.status == 'identical') {
-        html += ' <span title="Branch '+b+' is identical to tag '+version+'"><i class="fa fa-check"></i></span>';
+        html += ' <span title="Branch '+new_ref+' is identical to tag '+version+'"><i class="fa fa-check"></i></span>';
         state = 'ok';
         customkey = '13';
       } else {
-        html += ' <span title="Branch '+b+' has comparison status with tag '+version+' set to '+diff.status+'"><i class="fa fa-warning"></i></span>';
+        html += ' <span title="Branch '+new_ref+' has comparison status with tag '+version+' set to '+diff.status+'"><i class="fa fa-warning"></i></span>';
         state = 'unknown';
         customkey = '14';
       }

25bafe6288e3ae3f3ee7950dbc97bb294962e073
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-03-23 18:08:25 +0100

    Check if pin is set

--- plugins/pinned_version.js
+++ plugins/pinned_version.js
@@ -2,6 +2,8 @@ dashboard.pinned_version = function(name) {
   var r = repositories[name];
   var version;
   var url;
+  var html;
+  var state = 'ok';
   if (r.info.ref) {
     version = r.info.ref;
     if (/^[a-z0-9]+$/.test(version)) {
@@ -11,10 +13,14 @@ dashboard.pinned_version = function(name) {
       // Assume tag (or branch?)
       url = r.info.git+'/releases/tag/'+version;
     }
-  } else {
+    html = '<a href="'+url+'">'+version+'</a>';
+  } else if (r.info.version) {
     version = r.info.version;
     url = 'https://forge.puppetlabs.com/'+r.forge.current_release.module.owner.username+'/'+r.forge.name+'/'+version;
+    html = '<a href="'+url+'">'+version+'</a>';
+  } else {
+    html = 'N/A <i class="fa fa-warning" title="No pin set"></i>';
+    state = 'warning';
   }
-  var html = '<a href="'+url+'">'+version+'</a>';
-  updateCell(name, 'pinned_version', html);
+  updateCell(name, 'pinned_version', html, state);
 }

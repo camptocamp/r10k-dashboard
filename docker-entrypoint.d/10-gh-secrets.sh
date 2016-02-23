563591d4bf0619e4e2b3c0de974c801ea22685e2
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-02-23 17:41:46 +0100

    redirect sed

--- docker-entrypoint.d/10-gh-secrets.sh
+++ docker-entrypoint.d/10-gh-secrets.sh
@@ -3,6 +3,6 @@
 if test -n "${CLIENT_ID}"; then
   mkdir -p /var/www/html/r10k-dashboard/auth/
   sed -e "s/YOUR_CLIENT_ID/${CLIENT_ID}/;s/YOUR_CLIENT_SECRET/${CLIENT_SECRET}/" \
-    /var/www/html/r10k-dashboard/auth.php /var/www/html/r10k-dashboard/auth/index.php
+    /var/www/html/r10k-dashboard/auth.php > /var/www/html/r10k-dashboard/auth/index.php
   sed -i "s/YOUR_CLIENT_ID/${CLIENT_ID}/" /var/www/html/r10k-dashboard/index.html
 fi

39841aaaad6800db4c43d07779e1e27d551702e3
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-02-23 16:38:03 +0100

    Improve index.html

--- docker-entrypoint.d/10-gh-secrets.sh
+++ docker-entrypoint.d/10-gh-secrets.sh
@@ -4,4 +4,5 @@ if test -n "${CLIENT_ID}"; then
   mkdir -p /usr/share/nginx/html/r10k-dashboard/auth/
   sed -e "s/YOUR_CLIENT_ID/${CLIENT_ID}/;s/YOUR_CLIENT_SECRET/${CLIENT_SECRET}/" \
     /usr/share/nginx/html/r10k-dashboard/auth.php /usr/share/nginx/html/r10k-dashboard/auth/index.php
+  sed -i "s/YOUR_CLIENT_ID/${CLIENT_ID}/" /usr/share/nginx/html/r10k-dashboard/index.html
 fi

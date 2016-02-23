4f77826d5d13964be40842ef3328187a35c70404
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-02-23 16:34:42 +0100

    Generate auth/index.php

--- docker-entrypoint.d/10-gh-secrets.sh
+++ docker-entrypoint.d/10-gh-secrets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if test -n "${CLIENT_ID}"; then
-  sed -i "s/YOUR_CLIENT_ID/${CLIENT_ID}/" /usr/share/nginx/html/r10k-dashboard/auth.php
-  sed -i "s/YOUR_CLIENT_SECRET/${CLIENT_SECRET}/" /usr/share/nginx/html/r10k-dashboard/auth.php
+  mkdir -p /usr/share/nginx/html/r10k-dashboard/auth/
+  sed -e "s/YOUR_CLIENT_ID/${CLIENT_ID}/;s/YOUR_CLIENT_SECRET/${CLIENT_SECRET}/" \
+    /usr/share/nginx/html/r10k-dashboard/auth.php /usr/share/nginx/html/r10k-dashboard/auth/index.php
 fi
